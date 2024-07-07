4bb5aa3dc3a24211eff332c0f53e04ba6a742c58
Anvar <asnowflake777@yandex.ru>
2024-07-07 21:46:13 +0200

    changed message

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	http.HandleFunc("/",
 		func(w http.ResponseWriter, r *http.Request) {
-			_, _ = w.Write([]byte("Hello World"))
+			_, _ = w.Write([]byte("Hello Amazing World\n"))
 		},
 	)
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
